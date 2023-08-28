d774caa38abdc4c4a8306a6a3404e4d452addb3a
Romans S <romans.spilaks@gmail.com>
2023-08-28 23:57:00 +0300

    Set up env

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = []
+30.times do |i|
+  users << User.create(
+    name: "User #{i + 1}",
+    photo: "https://unsplash.com/photos/abcdef",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    posts_counter: rand(1..10)
+  )
+end
+
+users.each do |user|
+  user.posts_counter.times do |i|
+    Post.create(
+      author_id: user.id,
+      title: "Hello Post #{i + 1} by #{user.name}",
+      text: "Hi",
+      comments_counter: 0,
+      likes_counter: 0
+    )
+  end
+end
+
+User.all.each do |user|
+  user.posts.each do |post|
+    rand(0..5).times do
+      post.comments.create(
+        text: Faker::Lorem.sentence,
+        author: User.all.sample
+      )
+    end
+
+    post.update(likes_counter: rand(0..20))
+  end
+end
