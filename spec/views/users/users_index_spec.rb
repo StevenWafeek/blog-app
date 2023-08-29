20a85469c3ab761e5688bfa219ce628b2c4586a5
Romans S <romans.spilaks@gmail.com>
2023-08-29 20:04:15 +0300

    Update users_index_spec.rb to fit the updated model

--- spec/views/users/users_index_spec.rb
+++ spec/views/users/users_index_spec.rb
@@ -7,22 +7,19 @@ RSpec.feature 'User index page', type: :feature do
 
     visit users_path
 
-    expect(page).to have_content('List of all the users')
-
     within '.wrapper' do
-      expect(page).to have_selector('h1', text: 'Users')
       expect(page).to have_selector('ul li', count: 2)
 
       within page.find('ul li.userNameWrapper', match: :first) do
         expect(page).to have_css('img[alt="Profile P."]')
         expect(page).to have_link(user1.name, href: user_path(user1), class: 'userNameAnchor')
-        expect(page).to have_content("Number of posts: #{user1.posts_counter}")
+        expect(page).to have_content("Number of posts: #{user1.posts.count}")
       end
 
       within page.all('ul li.userNameWrapper').last do
         expect(page).to have_css('img[alt="Profile P."]')
         expect(page).to have_link(user2.name, href: user_path(user2), class: 'userNameAnchor')
-        expect(page).to have_content("Number of posts: #{user2.posts_counter}")
+        expect(page).to have_content("Number of posts: #{user2.posts.count}")
       end
     end
   end
