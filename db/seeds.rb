1df72937feadaea52bfae786636266ab936f7cd0
Romans S <romans.spilaks@gmail.com>
2023-08-29 19:55:54 +0300

    Update/fix seeds using body instead of text. Update show.html.erb view to render correct link and text from the post.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 User.destroy_all
 Post.destroy_all
 Comment.destroy_all
+Like.destroy_all
 
-User.transaction do
-  Comment.destroy_all
+users = []
+30.times do
+  users << User.create(
+    name: Faker::Name.name,
+    bio: Faker::Quote.most_interesting_man_in_the_world
+  )
+end
 
-  30.times do
-    user = User.create(
-      name: Faker::Name.name,
-      photo: "https://unsplash.com/photos/abcdef",
-      bio: Faker::Quote.most_interesting_man_in_the_world,
-      posts_counter: rand(1..40)
+users.each do |user|
+  rand(1..5).times do
+    post = user.posts.create(
+      title: Faker::Book.title,
+      body: Faker::Quote.famous_last_words,
     )
 
-    user.posts_counter.times do
-      post = Post.create(
-        author: user,
-        title: Faker::Book.title,
-        text: Faker::Quote.famous_last_words,
-        comments_counter: 0,
-        likes_counter: 0
+    rand(0..5).times do
+      post.comments.create(
+        text: Faker::Lorem.sentence,
+        author: users.sample
       )
-
-      rand(0..5).times do
-        post.comments.create(
-          text: Faker::Lorem.sentence,
-          author: User.all.sample
-        )
-      end
-
-      post.update(likes_counter: rand(0..100))
     end
+
+    post.update(likes_counter: rand(0..100))
   end
 end
