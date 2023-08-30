c93ceb4d891450063bd16d705fa2f521191e6551
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-30 13:03:29 +0300

    Update changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # has_one_attached :photo
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # has_one_attached :photo
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
