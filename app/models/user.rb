f99a7553a6d9def2a9c8ad1d2948dc4e42a21ef1
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-30 12:34:37 +0300

    Add device views bundle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   # has_one_attached :photo
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
