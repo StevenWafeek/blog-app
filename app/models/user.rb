ed37b953aaadde07661f2852a38506be5a190268
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-31 08:27:27 +0300

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable, :confirmable, :recoverable
 
-
-
   # has_one_attached :photo
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
